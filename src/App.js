53832d80a1652ce5b2567c7ae955e3603f1c8e11
Eliel Sobral <eliel.sobral@gmail.com>
2021-04-14 19:57:50 -0300

    Componentes de Classe com Statefull

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+ /* constructor(props) {
+    super(props);
+     this.handlePClick = this.handlePClick.bind(this);
+
+    this.state = { */
+
+  state = {
+    name: 'Eliel Sobral',
+    counter: 0
+  };
+
+  handlePClick = () => {
+    this.setState({name: 'Eliel Araujo Sobral'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+ 
+  render() {
+
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            { name } { counter }
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +69,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
