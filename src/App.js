1260ad911253567eb73b219d18b1e0371c09a9a5
Eliel Sobral <eliel.sobral@gmail.com>
2021-04-15 00:12:38 -0300

    Ciclo de vida dos componentes - 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-   posts: [
+    counter: 0,
+    posts: [
      {
        id: 1,
        title: 'Title 1',
@@ -21,14 +22,36 @@ class App extends Component {
     },
    ]
   };
+  timeoutUpdate = null;
 
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O título mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+  }
   
   render() {
 
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {
           posts.map( post => (
             <div key={post.id}>
